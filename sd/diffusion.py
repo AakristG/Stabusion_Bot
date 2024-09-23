57f0483c2dd925317f6da582b443039394dea008
Aakrist Godar <aakrist.godar.1@gmail.com>
2024-09-23 16:27:13 -0500

    diffusion finished
    implemented the UNET attention and residual block and the rest of the classes

--- sd/diffusion.py
+++ sd/diffusion.py
@@ -147,19 +147,158 @@ class UNET(nn.Module):
            
         ])
         
-
-class UNET_ResidualBlock(nn.Module):
-    {
+class UpSample(nn.Module):
+    
+    def __init__(self, channels: int):
+        super().__init__() 
+        self.conv = nn.Conv2d(channels, channels, kernel_siz=3, padding=1)
         
-    }
+    def forward(self, x):
+        # (Batch_Size, Features, Height, Width) -> (Batch_Size, Features, Height * 2, Width * 2 )
+        x = F.interpolate (x, scale_factor=2, mode="nearest") #doubles the size 
+        
+        return self.conv(x)
+class UNET_ResidualBlock(nn.Module):
+    
+    def __init__(self, in_channels: int, out_channels: int, n_time=1280):
+        super().__init__()
+        self.groupnom_feature = nn.GroupNorm(32, in_channels)
+        self.conv_feature = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.linear_time = nn.Linear(n_time, out_channels)
+        
+        self.groupnom_merged = nn.GroupNorm(32, out_channels)
+        self.conv_merged = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        
+        # can connect them with each other using the residual connection
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            #if not then we concer thte input into the output otherwise we can't add the tensors together
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padidng=1)
+            
+    def forward(self, feature, time):
+        # feature: (Batch_Size, In_Channels, Heigth, Width)
+        # time: (1, 1280)
+        
+        #we need the way to combine the latent, time, and the prompt embeddings together into the unit and pass it inot the UNET
+        
+        residue = feature
+        
+        feature = self.groupnom_feature(feature)
+        
+        feature  = F.silu(feature)
+        
+        feature = self.conv_feature(feature)
+        
+        time = F.silu(time)
+        
+        time = self.linear_time(time)
+        
+        merged = feature + time.unsqueeze(-1).unsqueeze(-1)
+        
+        merged = self.groupnom_merged(merged)
+        
+        merged = F.silu(merged)
+        
+        merged = self.conv_merged(merged)
+        
+        return merged + self.residual_layer(residue)
+        
+        
     
 class UNET_AttentionBlock(nn.Module):
-    {
+    
+    def __init__(self, n_head: int, n_embd: int, d_context=768):
+        super().__init__()
+        channels = n_head * n_head
         
-    }
+        self.groupnorm = nn.GroupNorm(32, channels, eps=1e-6)
+        self.conv_input = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
         
+        self.layernorm_1 = nn.LayerNorm(channels)
+        self.attention_1 = SelfAttention(n_head, channels, in_proj_bias=False)
+        self.layernorm_2 = nn.LayerNorm(channels)
+        self.attention_2 = CrossAttention(n_head, channels, d_context, in_proj_bias=False)
+        self.lienar_geglu_1 = nn.Linear(channels, 4 * channels * 2)
+        self.linear_geglu_2 = nn.Linear(4 * channels, channels)
+        
+        self.conv_output = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+        
+    def forward(self, x, context):
+        # x: (Batch_Size, Features, Height, Width)
+        # context: (Batch_Size, Seq_len, Dim)
+        
+        residue_long = x
+        
+        x = self.groupnorm(x)
+        
+        x = self.conv_input(x)
+        
+        #batch size, num of features, height, and width
+        n, c, h, w = x.shape
+        
+        x = x.view(n, c, h * w)
+
+        x = x.transpose(-1, -2)
+        
+        # apply normalization + self attention with skip connection
+        
+        residue_short = x
+        
+        x = self.layernorm_1(x)
+        self.attention_1(x)
+        x += residue_short
+        
+        
+        residue_short = x
+        
+        #Normalizaiton + cross attention with skip connection
+        x = self.layernorm_2(x)
+        
+        # Cross Attention
+        self.attention_2(x, context)
+        
+        x += residue_short
+        
+        residue_short = x
+        
+        #Normalization = feed forward with GeGLU and skip connection
+        x = self.layernorm_3(x)
+        
+        x, gate = self.linear_geglu_1(x).chunk(2, dim=-1)
+        x = x * F.gelu(gate)
+        
+        
+        x = self.linear_geglu_2(x)
+        
+        x += residue_short
+
+        # (Batch_size, Height * Width, Features) -> (Batch_Size, Features, Height * Width)
+        x = x.transpose(-1, -2)
+        
+        x = x.view((n,c,h,w))
+        
+        #defines the long skip connection
+        
+        return self.conv_output(x) + residue_long
 class UNET_OutputLayer(nn.Module):
-    {
+
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, in_channels)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+    
+    #@param x (Bath_Size, 320, Height /8, Width /8)
+    def forward(self, x):
         
-    }
+        #apply group normalization
+        x = self.groupnorm(x)
+        
+        #apply silu method 
+        x = F.silu(x)
+        
+        # apply convolution
+        x = self.conv(x)
+        
+        return x
     
